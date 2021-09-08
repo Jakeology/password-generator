14aa89a4938711cf0416771e2978ac3b630d6cd0
Jake <xxjakexx14@gmail.com>
2021-09-08 11:59:10 -0400

    added password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,33 @@ var uppercasedChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function getPassLength() {
+  var getPasswordLength = window.prompt("How many characters would you like your password? (8-128)");
+
+  if (getPasswordLength === null) {
+    return;
+  }
+
+  if(!isNaN(getPasswordLength)) {
+    if(getPasswordLength >= 8 && getPasswordLength <= 128) {
+      window.alert("You have choosen a password that meets the requirements");
+      return true;
+    } else {
+      window.alert("You have choosen a password that does not meets the requirements");
+      return getPassLength();
+    }
+  } else {
+    window.alert("Invalid input. Please put a valid number between 8-128");
+    return getPassLength();
+  }
+}
+
+function generatePassword() {
+  var getPasswordLength = getPassLength();
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
